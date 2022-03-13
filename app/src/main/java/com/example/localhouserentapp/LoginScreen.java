37ec2a3bb2c5c033c8fe73536efd41683ab4b27d
CharanBharathula <bcharan197@gmail.com>
2022-03-13 23:06:25 +0530

    Basic Screens like SignUp, Login, Splash Screen, home screen with basic activity were created

---
+++ app/src/main/java/com/example/localhouserentapp/LoginScreen.java
@@ -0,0 +1,37 @@
+package com.example.localhouserentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class LoginScreen extends AppCompatActivity {
+
+    TextView gotoRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_login_screen);
+
+        initialize();
+        gotoRegister.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginScreen.this, SignUpScreen.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+    }
+
+    private void initialize()
+    {
+        gotoRegister = findViewById(R.id.gotoRegister);
+    }
+}
